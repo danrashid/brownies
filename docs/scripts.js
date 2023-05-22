abd4a26eacc3d597bcb5525e1f0ae566fcd5cbf2
Dan Rashid <git@danrashid.com>
2023-05-22 18:10:47 -0500

    Use PKCE flow

--- docs/scripts.js
+++
@@ -1,5 +0,0 @@
-function getParam(name) {
-  var regex = new RegExp("[?&]" + name + "=([^?&]+)");
-  var matches = window.location.search.match(regex) || [];
-  return matches[1] && decodeURIComponent(matches[1]);
-}
