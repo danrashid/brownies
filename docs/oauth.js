86cb9bf72f680c11fd69f8bbdf6bc094d7ac0da4
Dan Rashid <git@danrashid.com>
2023-05-23 19:37:19 -0500

    Expose generateCodeChallenge and make state generic

--- docs/oauth.js
+++ docs/oauth.js
@@ -9,6 +9,21 @@ export function generateRandomString(length) {
   return text;
 }
 
+export async function generateCodeChallenge(codeVerifier) {
+  function base64encode(string) {
+    return btoa(String.fromCharCode.apply(null, new Uint8Array(string)))
+      .replace(/\+/g, "-")
+      .replace(/\//g, "_")
+      .replace(/=+$/, "");
+  }
+
+  const encoder = new TextEncoder();
+  const data = encoder.encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+
+  return base64encode(digest);
+}
+
 export function fetchWithErrorHandling(url, options, handleSuccess) {
   return fetch(url, options)
     .then((response) => {
@@ -25,20 +40,16 @@ export function fetchWithErrorHandling(url, options, handleSuccess) {
 
 export function parseCallbackParams() {
   const urlParams = new URLSearchParams(window.location.search);
-  let code = urlParams.get("code");
-  let [, clientId, redirectUri] = urlParams.get("state").split(",");
+  const code = urlParams.get("code");
+  const [_nonce, ...passthruParams] = urlParams.get("state").split(",");
 
-  return {
-    code,
-    clientId,
-    redirectUri,
-  };
+  return [code, ...passthruParams];
 }
 
-export function populateStateValue(redirectUri) {
+export function populateStateValue(...passthruParams) {
   return function () {
     const clientId = document.getElementById("client_id").value;
-    const state = [generateRandomString(16), clientId, redirectUri];
+    const state = [generateRandomString(16), clientId, ...passthruParams];
 
     document.getElementById("state").value = state;
   };
